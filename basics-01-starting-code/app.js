4e6c484b94cc6da99022b6a7e97347686de8f81a
Aitor Trillo Díaz <aitor@Aitors-iMac.local>
2023-01-03 11:09:13 +0100

    update

--- basics-01-starting-code/app.js
+++ basics-01-starting-code/app.js
@@ -2,11 +2,32 @@ const app = Vue.createApp({
     data() {
         return {
             message: 'Learn Vue',
+            courseGoalA: 'course goal A',
+            courseGoalB: 'course goal B',
+            rawHTML: '<h1>Hello</h1>',
             dictionary: {name: 'Aitor', age: 21, city: 'Granada'},
             array: ['Aitor', 21, 'Granada'],
             src: 'https://media.giphy.com/media/dVteEXB3aPER9ZYgFQ/giphy.gif',
             linkedin: "https://www.linkedin.com/in/aitortrillod%C3%ADaz/",
         };
+    }, 
+    methods: {
+        outputGoal() {
+            const randomNumber = Math.random();
+            if (randomNumber > 0.5) {
+                return 'Learn Vue!'
+            } else {
+                return 'Also Learn Vue.'
+            }
+        },
+        outputCourseGoal() {
+            const randomNumber = Math.random();
+            if (randomNumber > 0.5) {
+                return this.courseGoalA;
+            } else {
+                return this.courseGoalB;
+            }
+        }
     }
 });
 
